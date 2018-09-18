281fc2617a632e6fb7bc29aa0cc6791075093fb2
Kevin Pierce <kpierce@cedar1.cedar.computecanada.ca>
2018-09-18 16:06:53 -0700

    fixed training script

---
+++ lilgpujob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1               
+#SBATCH --mem=32000M             
+#SBATCH --time=00:05:00         
+#SBATCH --account=def-marwanh
+#SBATCH --mail-user=kevinkayaks@gmail.com
+#SBATCH --mail-type=ALL
+
+module load python/3.6  
+source $HOME/jupyter_py3/bin/activate
+python3 training_script.py
